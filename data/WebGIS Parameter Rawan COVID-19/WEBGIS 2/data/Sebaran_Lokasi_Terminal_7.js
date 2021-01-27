cb68fd50dc3fc4c9a653bcb7fb8255e6cf280644
giscondongcatur <umimunawwirotuz@mail.ugm.ac.id>
2021-01-27 17:03:41 +0700

    first commit

---
+++ data/WebGIS Parameter Rawan COVID-19/WEBGIS 2/data/Sebaran_Lokasi_Terminal_7.js
@@ -0,0 +1,15 @@
+var json_Sebaran_Lokasi_Terminal_7 = {
+"type": "FeatureCollection",
+"name": "Sebaran_Lokasi_Terminal_7",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "Nama": "Terminal Condongcatur", "Dusun": "Gejayan", "Desa": "Condongcatur", "Kecamatan": "Depok", "POINT_X": 433357.258187, "POINT_Y": 9142556.7671000008 }, "geometry": { "type": "Point", "coordinates": [ 110.395615817707608, -7.756654555345633 ] } },
+{ "type": "Feature", "properties": { "Nama": "Stasiun KA Maguwoharjo", "Dusun": "Kembang", "Desa": "Maguwoharjo", "Kecamatan": "Depok", "POINT_X": 437913.565879, "POINT_Y": 9139440.6166099999 }, "geometry": { "type": "Point", "coordinates": [ 110.436898084242685, -7.784897471361563 ] } },
+{ "type": "Feature", "properties": { "Nama": "Stasiun KA Patukan", "Dusun": "Patukan", "Desa": "Ambarketawang", "Kecamatan": "Gamping", "POINT_X": 425621.47020400001, "POINT_Y": 9138763.4329799991 }, "geometry": { "type": "Point", "coordinates": [ 110.325408184807159, -7.790859938804003 ] } },
+{ "type": "Feature", "properties": { "Nama": "Terminal Jombor", "Dusun": "Jombor", "Desa": "Sinduadi", "Kecamatan": "Mlati", "POINT_X": 429580.73687600001, "POINT_Y": 9143598.7420799993 }, "geometry": { "type": "Point", "coordinates": [ 110.361382101557766, -7.747179780648362 ] } },
+{ "type": "Feature", "properties": { "Nama": "Bandar Udara Adisutjipto", "Dusun": "Kembang", "Desa": "Sendangtirto", "Kecamatan": "Berbah", "POINT_X": 437992.28127899999, "POINT_Y": 9139368.7739400007 }, "geometry": { "type": "Point", "coordinates": [ 110.437611115106094, -7.785548250525324 ] } },
+{ "type": "Feature", "properties": { "Nama": "Terminal Pakem", "Dusun": "Sukunan", "Desa": "Pakembinangun", "Kecamatan": "Pakem", "POINT_X": 436061.49434799998, "POINT_Y": 9152495.0310200006 }, "geometry": { "type": "Point", "coordinates": [ 110.420262082785356, -7.666794973181334 ] } },
+{ "type": "Feature", "properties": { "Nama": "Terminal Kaliurang", "Dusun": "Kaliurang Timur", "Desa": "Hargobinangun", "Kecamatan": "Pakem", "POINT_X": 437365.31462600001, "POINT_Y": 9160650.9562999997 }, "geometry": { "type": "Point", "coordinates": [ 110.432180880038999, -7.593038225935454 ] } },
+{ "type": "Feature", "properties": { "Nama": "Terminal Prambanan", "Dusun": "Kranggan", "Desa": "Bokoharjo", "Kecamatan": "Prambanan", "POINT_X": 443704.95799600001, "POINT_Y": 9142660.0801500008 }, "geometry": { "type": "Point", "coordinates": [ 110.48945763465187, -7.755842971461322 ] } }
+]
+}
---
+++ data/WebGIS RS Rujukan COVID-19/WEBGIS 3/data/RumahSakitRujukanCOVID19_5.js
@@ -0,0 +1,17 @@
+var json_RumahSakitRujukanCOVID19_5 = {
+"type": "FeatureCollection",
+"name": "RumahSakitRujukanCOVID19_5",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "Nama": "RSUP Dr. Sardjito", "Alamat": "Jl. Kesehatan No.1, Senolowo, Sinduadi, Kec. Mlati, Kabupaten Sleman, Daerah Istimewa Yogyakarta 55281", "Hotline": "(0274) 631190" }, "geometry": { "type": "Point", "coordinates": [ 110.373473883095997, -7.76860624915489 ] } },
+{ "type": "Feature", "properties": { "Nama": "RSUD Sleman", "Alamat": "Jl. Bhayangkara No.48, Temulawak, Triharjo, Kec. Sleman, Kabupaten Sleman, Daerah Istimewa Yogyakarta 55514", "Hotline": "(0274) 868437" }, "geometry": { "type": "Point", "coordinates": [ 110.341897866924995, -7.68686685285986 ] } },
+{ "type": "Feature", "properties": { "Nama": "RSUD Prambanan", "Alamat": "Delegan, Jl. Raya Piyungan - Prambanan No.KM. 7, Dinginan, Sumberharjo, Kec. Prambanan, Kabupaten Sleman, Daerah Istimewa Yogyakarta 55572", "Hotline": "(0274) 4398570" }, "geometry": { "type": "Point", "coordinates": [ 110.483151959693998, -7.804625530576 ] } },
+{ "type": "Feature", "properties": { "Nama": "RS JIH", "Alamat": "Jl. Ring Road Utara No.160, Perumnas Condong Catur, Condongcatur, Kec. Depok, Kabupaten Sleman, Daerah Istimewa Yogyakarta 55283", "Hotline": "(0274) 4463535" }, "geometry": { "type": "Point", "coordinates": [ 110.403377866545, -7.7576258192591 ] } },
+{ "type": "Feature", "properties": { "Nama": "RS Panti Rini", "Alamat": "Jl. Raya Solo - Yogyakarta KM.13,2, Kringinan, Tirtomartani, Kec. Kalasan, Kabupaten Sleman, Daerah Istimewa Yogyakarta 55571", "Hotline": "(0274) 496022" }, "geometry": { "type": "Point", "coordinates": [ 110.466781326477005, -7.77217405116601 ] } },
+{ "type": "Feature", "properties": { "Nama": "RSU Sakina Idaman", "Alamat": "Jl. Blunyah Gede No.60, Kutu Dukuh, Sinduadi, Kec. Mlati, Kabupaten Sleman, Daerah Istimewa Yogyakarta 55284", "Hotline": "(0274) 8000111" }, "geometry": { "type": "Point", "coordinates": [ 110.368095648386003, -7.76764779016738 ] } },
+{ "type": "Feature", "properties": { "Nama": "RS PKU Muhammadiyah Gamping", "Alamat": "Jl. Wates, Jl. Nasional III KM.5,5, Bodeh, Ambarketawang, Kec. Gamping, Kabupaten Sleman, Daerah Istimewa Yogyakarta 55294", "Hotline": "(0274) 6499704" }, "geometry": { "type": "Point", "coordinates": [ 110.317710398068996, -7.80039826738429 ] } },
+{ "type": "Feature", "properties": { "Nama": "RS Bhayangkara", "Alamat": "Jl. Raya Solo - Yogyakarta KM.14, Glondong, Tirtomartani, Kec. Kalasan, Kabupaten Sleman, Daerah Istimewa Yogyakarta 55571", "Hotline": "(0274) 498278" }, "geometry": { "type": "Point", "coordinates": [ 110.471825191538997, -7.76620217304528 ] } },
+{ "type": "Feature", "properties": { "Nama": "RSA UGM", "Alamat": "Jl. Kabupaten, Kranggahan I, Trihanggo, Kec. Gamping, Kabupaten Sleman, Daerah Istimewa Yogyakarta 55291", "Hotline": "(0274) 4530404" }, "geometry": { "type": "Point", "coordinates": [ 110.350358985987995, -7.74293498558999 ] } },
+{ "type": "Feature", "properties": { "Nama": "RS Hermina", "Alamat": "Jl. Selokan Mataram, RT.06\/RW.50, Meguwo, Maguwoharjo, Kec. Depok, Kabupaten Sleman, Daerah Istimewa Yogyakarta 55282", "Hotline": "(0274) 2800808" }, "geometry": { "type": "Point", "coordinates": [ 110.432773789639995, -7.77024607390932 ] } }
+]
+}
